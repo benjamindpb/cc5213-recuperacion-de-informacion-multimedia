1b2288aeb035bd0382ec8dffe1012561df8ad6f0
benjamindpb <benjamin.delpino@ug.uchile.cl>
2021-04-12 02:23:03 -0400

    Se comienza a trabajar en la busqueda

--- tarea1-buscar.py
+++ tarea1-buscar.py
@@ -1,5 +1,8 @@
 import sys
 import os.path
+import cv2
+from matplotlib import pyplot as plt
+import pickle
 
 if len(sys.argv) < 3:
     print("Uso: {} [dataset_q] [datos] [resultados]".format(sys.argv[0]))
@@ -17,6 +20,40 @@ if not os.path.isdir(datos):
     print("no existe directorio {}".format(datos))
     sys.exit(1)
 
-# buscar descriptores
+'''
+    Se abre el fichero binario que fue calculado en el procesamiento
+    de las imagenes para utilizar su informacion.
+'''
+fichero = open("datos_R/dict_images", "rb")
+dict_images_r = pickle.load(fichero) # se cargan los datos
+
+dict_images_q = {}
+
+for image_path in os.listdir(dataset_q):
+    img = cv2.imread(dataset_q + image_path, cv2.IMREAD_GRAYSCALE) # or just 0
+    # se obtienen las dimensiones de la imagen
+    height, width = img.shape
+    '''
+        Para calcular los descriptores de las imagenes de Q se hace
+        lo mismo que se hizo en el procesamiento de las imagenes de R
+    '''
+    imgs = [0]*4
+    hist = [0]*4
+    for i in range(4):
+        imgs[i] = img[(height//4)*i:(height//4)*(i+1),:]
+    for i in range(4):
+        hist[i] = cv2.calcHist(imgs[i], [0], None, [64], [0,256]) # 32 - 64
+    dict_images_q[image_path] = hist
+
+for key in dict_images_q:
+    print(cv2.compareHist(dict_images_q[key][1],dict_images_q[key][3],cv2.HISTCMP_CHISQR)) # 1 
+    break
+
 # escribir en resultados
-print("no implementado!")
+
+'''
+    Lo que hice fue dividir la imagen horizontalmente en 4 imagenes. Luego 
+    para cada imagen calcule su histograma. Es decir a cada imagen le corresponde un ARRAY
+    de 4 histogramas. ¿Como los comparo ahora con los 4 histogramas de las imagenes en Q? 
+    ¿Como puedo determinar solo UNA distancia si tengo 4? ¿Hago un promedio? ¿Las sumo? D:
+'''
--- tarea1-procesar.py
+++ tarea1-procesar.py
@@ -2,6 +2,7 @@ import sys
 import os.path
 import cv2
 from matplotlib import pyplot as plt
+import pickle
 
 if len(sys.argv) < 2:
     print("Uso: {} [dataset_r] [datos]".format(sys.argv[0]))
@@ -19,35 +20,36 @@ if not os.path.isdir(datos):
     print("creando directorio {}".format(datos))
     os.mkdir(datos)
 
-# calcular descriptores (histogramas)
+# Diccionario que almacenara los descriptores de las imagenes
+dict_images_r = {}
 
-images_folder = os.listdir(dataset_r)
-dict_images = {}
-
-# # primero se itera en la carpeta que contiene las imagenes originales
-for image_path in images_folder:
-    # se lee la imagen es escala de grises
+# Se itera en la carpeta que contiene las imagenes
+for image_path in os.listdir(dataset_r):
     img = cv2.imread(dataset_r + image_path, cv2.IMREAD_GRAYSCALE) # or just 0
-    height, width = img.shape # se obtienen las dimensiones de la imagen
-
-    # # se procede a calcular 4
+    # se obtienen las dimensiones de la imagen
+    height, width = img.shape
+    '''
+        Se divide horizontalmente la imagen original en 4 imagenes. A cada
+        una de estas imagenes se le calcula su respectivo histograma para luego
+        agregar al diccionario el path de la imagen con sus respectivos 
+        histogramas.
+    '''
     imgs = [0]*4
     hist = [0]*4
     for i in range(4):
         imgs[i] = img[(height//4)*i:(height//4)*(i+1),:]
-
     for i in range(4):
-        hist[i] = cv2.calcHist(imgs[i], [0], None, [4], [0,256]) # 32 - 64
-        
+        hist[i] = cv2.calcHist(imgs[i], [0], None, [64], [0,256]) # 32 - 64
+    dict_images_r[image_path] = hist
 
-    dict_images[image_path] = hist
+'''
+    Ahora que se tiene el dict con las imagenes y sus descriptores,
+    se procede a codificar el dict en un archivo binario externo
+    para poder utilizar su contenido en el archivo de busqueda.
+    Para llevar a cabo esto se utiliza la libreria *pickle*.
+'''
+fichero_binario = open("datos_R/dict_images_r", "wb") # se crea el fichero
+# Se "vuelca" la informacion del diccionario en el fichero
+pickle.dump(dict_images_r, fichero_binario)
 
-
-
-
-    
-    
-    
- 
-
-
+fichero_binario.close() # Cerramos el fichero
