195f1218bb0bc7952ab4b4b0b8835393d39e8e2e
benjamindpb <benjamin.delpino@ug.uchile.cl>
2021-04-29 03:40:25 -0400

    se implementa extraccion de la pista de audio

--- Tarea 2/tarea2-extractor-audio.py
+++ Tarea 2/tarea2-extractor-audio.py
@@ -1,5 +1,6 @@
 import sys
 import os.path
+import subprocess
 
 if len(sys.argv) < 3:
     print("Uso: {} [carpeta_entrada] [carpeta_salida]".format(sys.argv[0]))
@@ -12,6 +13,11 @@ if not os.path.isdir(carpeta_entrada):
     print("no existe directorio {}".format(carpeta_entrada))
     sys.exit(1)
 
+# se crea la carpeta de los datos de las pistas de audio
+if not os.path.isdir(carpeta_salida):
+    print("creando directorio {}".format(carpeta_salida))
+    os.mkdir(carpeta_salida)
+
 #procesar todos los archivos de carpeta_entrada que terminen en (mp4 avi mpg wav mp3)
 #para cada archivo:
 #  extraer la pista de audio
@@ -19,3 +25,26 @@ if not os.path.isdir(carpeta_entrada):
 #  calcular una matriz de descriptores de N filas por d columnas (d=dimension del descriptor)
 #  crear directorio: os.mkdir(carpeta_salida)
 #  escribir matriz descriptores en carpeta_salida/archivo
+
+sample_rate = 8192
+segmentos = 10
+L = []
+
+for file_path in os.listdir(carpeta_entrada):
+    # nombre que se le da al audio
+    file_wav = "{}.wav".format(file_path)
+    # este comando se encarga de extraer la pista de audio
+    comando = ["ffmpeg", "-i", carpeta_entrada + "/" + file_path, "-ac", "1", "-ar", str(sample_rate), carpeta_salida + "/" + file_wav]
+    # la siguiente linea genera un nuevo proceso que ejecuta el comando para 
+    # extraer la pista de audio
+    code = subprocess.call(comando)
+    if code != 0:
+        raise Exception("ERROR!")
+    """
+        Ahora se debe dividir el audio en segmentos de largo fijo y calcular
+        un único vector por segmento, luego se juntan (?)
+
+        MFCC (?)
+    """
+
+    break
